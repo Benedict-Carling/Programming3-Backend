fa77e58ce5da799efebc8bb21a67e5165ce58d57
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-11-30 14:33:57 +0000

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const { response } = require('express');
+const jwt = require('jsonwebtoken')
+
+const auth = (req, res, next) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token)
+            return response.status(401).json({msg: "No authentication token, authentication denied"})
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!token)
+            return response.status(401).json({msg: "Token verification failed, authentication denied"})
+
+        req.user = verified.id
+        next()
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+
+}
+
+module.exports = auth;
