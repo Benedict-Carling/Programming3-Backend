bfb1ce824dac75fb191739299d793745b18111f4
Abdullah Rehman <ayr18@ic.ac.uk>
2020-12-08 13:38:39 +0000

    Setting up main table load functionality

---
+++ models/dataModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+    U_PASSCODE: { type: String, required: true, unique: true},
+    Id: { type: String, required:true, unique: true },
+    Id: { type: String, required:true, unique: true },
+    Date: { type: String, required:true},
+    Flag: { type: String, required:true},
+    "User interpretation": { type: String, required:true},
+    "Algorithm interpretation": { type: String, required:true},
+    "Expert interpretation": { type: String},
+    "Expert comment": { type: String},
+    "Image path": { type: String, required:true},
+});
+
+module.exports = Data = mongoose.model("data",dataSchema);
+module.exports = Data;
