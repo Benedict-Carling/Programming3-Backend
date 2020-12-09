7fb3bd47089751e2e7d75ba1671d64e45a93219e
JamesMarshall31 <JM7618@ic.ac.uk>
2020-12-09 13:44:44 +0000

    Database update
    currently only updates id 10 and expertcomment and expertinterpretation wont change

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -13,5 +13,29 @@ router.get("/table", async(req,res) => {
         res.status(500).json(err.message);
       }
 });
+/*U_PASSCODE: row.U_PASSCODE,
+          Id: row.Id,
+          Date: row.Date,
+          Flag: row.Flag,
+          UserInterpretation: row.UserInterpretation,
+          AlgorithmInterpretation: row.AlgorithmInterpretation,
+          ExpertInterpretation: row.ExpertInterpretation,*/
+var id = "10";
+//ImagePath: row.ImagePath,
+router.post("/add-comment", async (req,res) =>{
+  try {
+    const row = await Data.findOne({"Id": id})
+    await Data.updateOne({"Id": id},
+    {$set:{  ExpertComment:"yes"}}
+     );
+   const updaterow = await Data.findOne({"Id": id})
+   res.status(200).json(updaterow)
+   console.log(updaterow.ExpertComment)
+   console.log(updaterow.Id)
+}
+   catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
 module.exports = router;
