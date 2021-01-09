1fc82f70af0c08c5c7d47ecc5595b446e5d12838
JamesMarshall31 <JM7618@ic.ac.uk>
2021-01-09 14:30:45 +0000

    Backend changes for adding U_PASSCODE to logs

--- models/logModel.js
+++ models/logModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 // Set up the mongoose schema that will be used to access the database
 const dataSchema = new mongoose.Schema({
     Email: {type: String, required: true},
+    U_PASSCODE:{type: String, required: true},
     accountType: {type: String, required: true},
     LogDate: { type: String, required:true},
     testId: { type: String, required:true},
