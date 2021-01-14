ccbef1f19901ffcc38807a8f4a1588b53d0ba788
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-14 18:45:33 +0000

    reformatts unit test using prettier

--- test/dataRead_test.js
+++ test/dataRead_test.js
@@ -1,30 +1,27 @@
-
 //inside read_test.js
-const assert = require('assert');
-const tester = require('../models/dataModel').tester; //imports the Data model.
+const assert = require("assert");
+const tester = require("../models/dataModel").tester; //imports the Data model.
 let testData;
 beforeEach(() => {
-        testData = new tester({
-        U_PASSCODE: "Test Passcode",
-        Id: "Test Id",
-        Date: "Test Date",
-        Flag: "Test Flag",
-        UserInterpretation: "Test user interpretation",
-        AlgorithmInterpretation: "Test Algorithm interpretation",
-        ExpertInterpretation: "Test Expert interpretation",
-        ExpertComment: "Test Expert Comment",
-        ImagePath: "Test Path"
-      });
-    testData.save()
-        .then(() => done());
+  testData = new tester({
+    U_PASSCODE: "Test Passcode",
+    Id: "Test Id",
+    Date: "Test Date",
+    Flag: "Test Flag",
+    UserInterpretation: "Test user interpretation",
+    AlgorithmInterpretation: "Test Algorithm interpretation",
+    ExpertInterpretation: "Test Expert interpretation",
+    ExpertComment: "Test Expert Comment",
+    ImagePath: "Test Path",
+  });
+  testData.save().then(() => done());
 });
-describe('Reading database details', () => {
-    it('finds entry with Id of Test Id', (done) => {
-        console.log(tester.findOne({ Id: 'Test Id' }))
-        tester.findOne({ Id: 'Test Id' })
-            .then((tester) => {
-                assert(testData.Id === 'Test Id'); 
-                done();
-            });
-    })
-})
+describe("Reading database details", () => {
+  it("finds entry with Id of Test Id", (done) => {
+    console.log(tester.findOne({ Id: "Test Id" }));
+    tester.findOne({ Id: "Test Id" }).then((tester) => {
+      assert(testData.Id === "Test Id");
+      done();
+    });
+  });
+});
